2c079c3a70e7cb93947860f0c7c05f2c9eac5976
Dean Davis <dean.davis@safholland.com>
2026-03-10 14:46:46 -0400

    Basic Climber Subsystem

---
+++ src/main/java/frc/robot/subsystems/climber/Climber.java
@@ -0,0 +1,38 @@
+package frc.robot.subsystems.climber;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.littletonrobotics.junction.Logger;
+
+public class Climber extends SubsystemBase {
+  private final ClimberIO io;
+  private final ClimberIOInputsAutoLogged inputs = new ClimberIOInputsAutoLogged();
+
+  public Climber(ClimberIO io) {
+    this.io = io;
+  }
+
+  public void periodic() {
+    io.updateInputs(inputs);
+    Logger.processInputs("Climber", inputs);
+  }
+
+  public void deployClimber() {
+    io.deployClimber();
+  }
+
+  public void stopClimber() {
+    io.stopClimber();
+  }
+
+  public void climb() {
+    io.climb();
+  }
+
+  public boolean isClimberDeployed() {
+    return io.isClimberDeployed();
+  }
+
+  public boolean isClimbingFinished() {
+    return io.isClimbingFinished();
+  }
+}
