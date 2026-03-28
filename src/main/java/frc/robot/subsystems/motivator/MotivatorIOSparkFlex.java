a8b477ca84b499ca765571d8a70981277134d5f2
Team 3572 <team3572@gmail.com>
2026-03-28 13:23:51 -0400

    Better Launcher and Motivator Stop Code

--- src/main/java/frc/robot/subsystems/motivator/MotivatorIOSparkFlex.java
+++ src/main/java/frc/robot/subsystems/motivator/MotivatorIOSparkFlex.java
@@ -165,13 +165,21 @@ public class MotivatorIOSparkFlex implements MotivatorIO {
 
   @Override
   public void setMotivatorVelocity(double wheelVelocityRPM) {
-    motivatorVelocityMode = true;
-    wheelTargetRPM = Math.abs(wheelVelocityRPM);
-    // ks & kv were calculated in motor RPM thus the conversion in the parameter
-    double arbFFVolts = feedforward.calculate(wheelToMotorRPM(wheelTargetRPM));
-    // PID control is also in motor rotations
-    motivatorController.setSetpoint(
-        wheelToMotorRPM(wheelTargetRPM), ControlType.kVelocity, ClosedLoopSlot.kSlot0, arbFFVolts);
+    if (wheelVelocityRPM < 1.0) {
+      motivatorVelocityMode = false;
+      wheelTargetRPM = 0.0;
+      motivator.stopMotor();
+    } else {
+      motivatorVelocityMode = true;
+      wheelTargetRPM = Math.abs(wheelVelocityRPM);
+      // ks & kv were calculated in motor RPM thus the conversion in the parameter
+      double arbFFVolts = feedforward.calculate(wheelToMotorRPM(wheelTargetRPM));
+      // PID control is also in motor rotations
+      motivatorController.setSetpoint(
+          wheelToMotorRPM(wheelTargetRPM), ControlType.kVelocity, ClosedLoopSlot.kSlot0, arbFFVolts);
+
+    }
+    
   }
 
   @Override
