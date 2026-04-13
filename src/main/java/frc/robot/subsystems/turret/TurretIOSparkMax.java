8ee8826aaa043c03f9e41dea9d8536e48cee025a
Team 3572 <team3572@gmail.com>
2026-04-13 17:31:28 -0400

    Go to 40% on the turret

--- src/main/java/frc/robot/subsystems/turret/TurretIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/turret/TurretIOSparkMax.java
@@ -63,7 +63,7 @@ public class TurretIOSparkMax implements TurretIO {
 
   private double currentInsideAngleDegrees;
   private double currentOutsideAngleDegrees;
-  private static double constCurrentOutputLimit = 0.3;
+  private static double constCurrentOutputLimit = 0.4;
 
   private static final LoggedTunableNumber turretOutputLimit =
       new LoggedTunableNumber("Tuning/Turret/outputLimit", constCurrentOutputLimit);
