ec82d68f8cb7d97d38d08e6c1acd1e02c1399533
Team 3572 <team3572@gmail.com>
2026-04-22 20:45:52 -0400

    Methods to set hood by voltage

--- src/main/java/frc/robot/subsystems/hood/HoodIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/hood/HoodIOSparkMax.java
@@ -149,4 +149,11 @@ public class HoodIOSparkMax implements HoodIO {
     targetAngle = angleDeg;
     motorController.setSetpoint(degreesToMotorRotations(targetAngle), ControlType.kPosition);
   }
+
+  @Override
+  public void setHoodVolts(double volts) {
+    if (volts > .5) volts = .5;
+    // Convert degrees to motor rotations for the PID controller
+    motorController.setSetpoint(volts, ControlType.kVoltage);
+  }
 }
